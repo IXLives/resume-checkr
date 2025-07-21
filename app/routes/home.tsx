92c7b3915c313b1c1dcb73137e52c3120ebbd62b
IXLives <bryant.ian87@gmail.com>
2025-07-21 14:11:42 -0400

    resumecard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,20 +1,31 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Resume Checkr" },
+    { title: "Resume Genius" },
     { name: "description", content: "Smart resume feedback" },
   ];
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <section className="main-section">
-      <div className="page-heading">
-        <h1>Track Your Applications</h1>
-        <h2>Review your submissions and get AI-powered feedback.</h2>
-      </div>
-    </section>
-  </main>;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications</h1>
+          <h2>Review your submissions and get AI-powered feedback.</h2>
+        </div>
+
+        {resumes.length > 0 &&
+          resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+      </section>
+    </main>
+  );
 }
