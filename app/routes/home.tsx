939eb70f71f0d819927a38d58a9bfc9fa636621f
IXLives <bryant.ian87@gmail.com>
2025-07-21 13:31:11 -0400

    landing

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,11 +3,18 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Checkr" },
+    { name: "description", content: "Smart resume feedback" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications</h1>
+        <h2>Review your submissions and get AI-powered feedback.</h2>
+      </div>
+    </section>
+  </main>;
 }
